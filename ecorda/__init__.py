4ac2848d404641af8fd4f7ecd7e8c14df2a23b52
folland87 <frederic@olland.me>
2024-03-18 13:42:54 +0100

    test

--- ecorda/__init__.py
+++ ecorda/__init__.py
@@ -42,7 +42,7 @@ def run_ecorda_pipeline() -> None:
         # 'projects/euroSciVocTaxonomy',
         # 'projects/reporting/publications',
         # 'projects/hrpResult',
-        # 'projects/cascadingProjects',
+        'projects/cascadingProjects',
         'projects/participants/cascadingParticipants',
         # 'projects/innovationRadar',
         # 'projects/datasets'
